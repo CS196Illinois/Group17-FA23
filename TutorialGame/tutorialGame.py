8444d9f41b1da81033e0aae8d53e2df0f259264a
lduyang2 <lduyang2@illinois.edu>
2023-12-02 20:38:30 -0600

    changes

--- TutorialGame/tutorialGame.py
+++ TutorialGame/tutorialGame.py
@@ -7,6 +7,8 @@ pygame.init()
 
 # Creating the window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+background = pygame.transform.scale(pygame.image.load("Sprites/Maps/blue.png").convert(), (1280,720)) 
+boundary = pygame.Rect(67, 65, 1147, 593)
 pygame.display.set_caption("Top Down Shooter")
 clock = pygame.time.Clock()
 
@@ -16,9 +18,21 @@ background = pygame.transform.scale(pygame.image.load("Sprites/Maps/blue.png").c
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load("Sprites/Characters/character_main.png").convert_alpha(), 0, PLAYER_SIZE)
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.image = pygame.transform.rotozoom(pygame.image.load("Sprites/Characters/character_main.png").convert_alpha(), 0, PLAYER_SIZE)
+        self.base_player_image = self.image
+        self.hitbox_rect = self.base_player_image.get_rect(center = self.pos)
+        self.rect = self.hitbox_rect.copy()
         self.speed = PLAYER_SPEED
+
+    def player_rotation(self):
+        self.mouse_coords = pygame.mouse.get_pos()
+        self.x_change_mouse_player = (self.mouse_coords[0] - self.hitbox_rect.centerx)
+        self.y_change_mouse_player = (self.mouse_coords[1] - self.hitbox_rect.centery)
+        self.angle = math.degrees(math.atan2(self.y_change_mouse_player, self.x_change_mouse_player))
+        self.image = pygame.transform.rotate(self.base_player_image, -self.angle)
+        self.rect = self.image.get_rect(center = self.hitbox_rect.center)
+       
 
     def user_input(self):
         self.velocity_x = 0
@@ -39,12 +53,39 @@ class Player(pygame.sprite.Sprite):
             self.velocity_x /= math.sqrt(2)
             self.velocity_y /= math.sqrt(2)
 
+        new_x = self.pos.x + self.velocity_x
+        new_y = self.pos.y + self.velocity_y
+
+        self.pos.x = new_x
+        self.pos.y = new_y
+
+        if self.pos.x < boundary.left: # Check left boundary
+            self.pos.x = boundary.left
+
+        if self.pos.x > boundary.right: # Check right boundary
+            self.pos.x = boundary.right
+        
+        if self.pos.y < boundary.top: # Check top boundary
+            self.pos.y = boundary.top
+        
+        if self.pos.y > boundary.bottom: # Check bottom boundary
+            self.pos.y = boundary.bottom
+
+        if pygame.mouse.get_pressed() == (1, 0, 0) or keys[pygame.K_SPACE]:
+            self.shoot = True
+            self.is_shooting()
+        else:
+            self.shoot = False
+
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        self.hitbox_rect.center = self.pos
+        self.rect.center = self.hitbox_rect.center
 
     def update(self):
         self.user_input()
         self.move()
+        self.player_rotation()
 
 player = Player()
 
@@ -56,8 +97,10 @@ while True:
             exit()
 
     screen.blit(background, (0, 0))
-    screen.blit(player.image, player.pos)
+    screen.blit(player.image, player.rect)
     player.update()
+    pygame.draw.rect(screen, "red", player.hitbox_rect, width=2)
+    pygame.draw.rect(screen, "yellow", player.rect, width=2)
 
     pygame.display.update()
     clock.tick(FPS)
